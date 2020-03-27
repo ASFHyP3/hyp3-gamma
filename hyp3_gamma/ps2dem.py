59c325f6890def9f3948ebefdd4d2d9c7c4fc71a
Joseph H Kennedy <me@jhkennedy.org>
2020-03-26 19:04:01 -0800

    Merge remote-tracking branch 'upstream/test' into test-2-dev
    * upstream/test:
      Added routine from python_data_utils
      New routine to simplify shape files added
      Added DEM cleaning
      Cleaned up a little
      Fixing dateline issues
      Minor code rearrangment
      Restoring ASF state vector fetching.
      Fix bad percentile value
      Add platform name to ESA query
      Results is never None.
      Tightening up the orbit fetching since it was erroring
      Wanted a float
      Forgot to change kwarg from float to real...
      Forgot to change config files!
      Typo
      Still debugging new DEM inclusions.
      More fixes for get_dem
      Finishing touches on new DEMs
      Use new EU_DEM_V11 with compression

--- hyp3_gamma/ps2dem.py
+++ hyp3_gamma/ps2dem.py
@@ -94,12 +94,11 @@ def ps2dem(inDem,outDem,demPar):
     # Convert to ENVI (binary) format
     tmptif = "temporary_dem_file.tif"
     if data.dtype == np.float32:
-        fdata = data.byteswap()
-#    elif data.type == np.int16:
+        fdata = data
     else:
         # Convert to floating point
         fdata = data.astype(np.float32)
-        fdata = fdata.byteswap()
+    fdata = fdata.byteswap()
     saa.write_gdal_file_float(tmptif,trans,proj,fdata)
     gdal.Translate(outDem,tmptif,format="ENVI")
     os.remove(tmptif)
