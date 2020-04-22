be1a63ca3116d85fbd1bde2805292f5aaf045b70
Joseph H Kennedy <me@jhkennedy.org>
2020-04-21 20:07:58 -0800

    Fix unused import and reference before assignment in getParameter

--- hyp3_gamma/getParameter.py
+++ hyp3_gamma/getParameter.py
@@ -1,24 +1,24 @@
 from __future__ import print_function, absolute_import, division, unicode_literals
 
 import logging
-import os
 import re
 
 
-def getParameter(parFile,parameter,uselogging=False):
+def getParameter(parFile, parameter, uselogging=False):
     """Read a value from a par file"""
 
     try:
-        myfile = open(parFile,"r")
+        myfile = open(parFile, "r")
     except IOError: 
-        if (uselogging):
+        if uselogging:
             logging.error("Unable to find file {}".format(parFile))
         raise Exception("ERROR: Unable to find file {}".format(parFile))
 
+    value = None
     parameter = parameter.lower()
     for line in myfile:
         if parameter in line.lower():
-            t = re.split(":",line)
+            t = re.split(":", line)
             value = t[1].strip()
     myfile.close()
 
@@ -27,5 +27,5 @@ def getParameter(parFile,parameter,uselogging=False):
             return value
     except:    
         if uselogging:
-            logging.error("Unable to find parameter {} in file {}".format(parameter,parFile))
-        raise Exception ("ERROR: Unable to find parameter {} in file {}".format(parameter, parFile))
+            logging.error("Unable to find parameter {} in file {}".format(parameter, parFile))
+        raise Exception("ERROR: Unable to find parameter {} in file {}".format(parameter, parFile))
