e959e64d766baa9fc2b7a80e65bb35cd5b1ec2b1
Joseph H Kennedy <me@jhkennedy.org>
2020-04-21 19:54:59 -0800

    Merge remote-tracking branch 'upstream/test' into test-2-dev
    * upstream/test:
      Skip X padding for EU_DEM
      Added my padding REM, GIMP, and EU_DEM
      Added get_dem_tile_lists
      iIx syntax
      Reproject all the images!
      Fiddling with parameters for DEM buffers.
      Fixing a bunch of problems

--- hyp3_gamma/getParameter.py
+++ hyp3_gamma/getParameter.py
@@ -8,16 +8,13 @@ import re
 def getParameter(parFile,parameter,uselogging=False):
     """Read a value from a par file"""
 
-    if os.path.isfile(parFile):
+    try:
         myfile = open(parFile,"r")
-    else: 
+    except IOError: 
         if (uselogging):
-            logging.error("ERROR: Unable to find file {}".format(parFile))
-        else:
-            print("ERROR: Unable to file file {}".format(parFile))
-        exit(1)
+            logging.error("Unable to find file {}".format(parFile))
+        raise Exception("ERROR: Unable to find file {}".format(parFile))
 
-    value = None
     parameter = parameter.lower()
     for line in myfile:
         if parameter in line.lower():
@@ -25,11 +22,10 @@ def getParameter(parFile,parameter,uselogging=False):
             value = t[1].strip()
     myfile.close()
 
-    if value is None:
+    try:
+        if value:
+            return value
+    except:    
         if uselogging:
-            logging.error("ERROR: Unable to find parameter {} in file {}".format(parameter,parFile))
-        else:
-            print("ERROR: Unable to find parameter {} in file {}".format(parameter, parFile))
-        exit(1)
-    return value
-
+            logging.error("Unable to find parameter {} in file {}".format(parameter,parFile))
+        raise Exception ("ERROR: Unable to find parameter {} in file {}".format(parameter, parFile))
