7280e321eaaaf4fc87a26a67a086409b9b8c413b
Joseph H Kennedy <me@jhkennedy.org>
2020-04-21 16:18:51 -0800

    Bring in proc script from cloud proj and set as __main__

--- hyp3_rtc_gamma/__main__.py
+++ hyp3_rtc_gamma/__main__.py
@@ -3,52 +3,280 @@ rtc_gamma processing for HyP3
 """
 
 import os
+import shutil
 from datetime import datetime
 
-import hyp3proclib
+from hyp3proclib import (
+    add_browse,
+    build_output_name,
+    clip_tiffs_to_roi,
+    execute,
+    extra_arg_is,
+    get_extra_arg,
+    failure,
+    find_browses,
+    process,
+    record_metrics,
+    success,
+    unzip,
+    upload_product,
+    zip_dir,
+    user_ok_for_jers
+)
+from hyp3proclib.db import get_db_connection
+from hyp3proclib.file_system import add_citation, cleanup_workdir
 from hyp3proclib.logger import log
 from hyp3proclib.proc_base import Processor
 
 import hyp3_rtc_gamma
 
 
-def hyp3_process(cfg, n):
+def find_png(dir_):
+    # First try to find RGB
+    for subdir, dirs, files in os.walk(dir_):
+        for file in files:
+            filepath = os.path.join(subdir, file)
+            if filepath.endswith(".png") and 'rgb' in filepath and 'large' not in filepath:
+                log.info('Browse image: ' + filepath)
+                return filepath
+
+    # No RGB, see if we can find a grayscale browse
+    for subdir, dirs, files in os.walk(dir_):
+        for file in files:
+            filepath = os.path.join(subdir, file)
+            if filepath.endswith(".png") and 'large' not in filepath:
+                log.info('Browse image: ' + filepath)
+                return filepath
+
+    return None
+
+
+def find_raw(dir_):
+    for subdir, dirs, files in os.walk(dir_):
+        for file in files:
+            filepath = os.path.join(subdir, file)
+            if filepath.endswith(".raw") and 'L0' in file:
+                log.info('Found L0 data file: ' + filepath)
+                return filepath
+
+    return None
+
+
+def find_zip(dir_):
+    for subdir, dirs, files in os.walk(dir_):
+        for file in files:
+            filepath = os.path.join(subdir, file)
+            if filepath.endswith(".zip") and ('GRD' in file or 'SLC' in file or 'L0' in file):
+                log.info('Found zip: ' + filepath)
+                return filepath
+
+    return None
+
+
+def find_and_remove(dir_, s):
+    for subdir, dirs, files in os.walk(dir_):
+        for file in files:
+            filepath = os.path.join(subdir, file)
+            if s in file:
+                log.info('Removing from PRODUCT dir: ' + filepath)
+                os.remove(filepath)
+
+
+def find_product(dir_):
+    for subdir, dirs, files in os.walk(dir_):
+        for d in dirs:
+            if d == "PRODUCT":
+                return os.path.join(subdir, d)
+
+    return None
+
+
+def download(cfg, granule):
+    if granule.startswith('S1'):
+        if 'GRD' in granule:
+            typ = "GRD"
+        elif 'SLC' in granule:
+            typ = "SLC"
+        else:
+            raise Exception("Unsupported Sentinel-1: "+granule)
+    else:
+        typ = "l0"
+
+    log.info('Downloading {0} with get_asf.py'.format(granule))
+    execute(cfg, "get_asf.py --{0} --dir={1} {2}".format(typ, cfg['workdir'], granule))
+
+    if 'GRD' in granule or 'SLC' in granule:
+        zip_file = os.path.join(cfg['workdir'], granule+'.zip')
+    else:
+        zip_file = find_zip(cfg['workdir'])
+        if zip_file:
+            granule = os.path.splitext(os.path.basename(zip_file))[0]
+        else:
+            log.info('Nothing found for {0}'.format(granule))
+            return granule
+
+    if not os.path.isfile(zip_file):
+        raise Exception('Could not find expected download file: ' + zip_file)
+    else:
+        log.info('Download complete')
+        log.info('Unzipping ' + zip_file)
+
+        unzip(zip_file, cfg['workdir'])
+
+        if granule.startswith('S1'):
+            safe_file = os.path.join(cfg['workdir'], granule+'.SAFE')
+            if not os.path.isdir(safe_file):
+                raise Exception('Failed to unzip, SAFE directory not found: ' + safe_file)
+
+    log.info('Unzip completed.')
+
+    return granule
+
+
+def process_rtc_gamma(cfg, n):
     try:
-        log.info('Processing hello_world')
-        if not cfg['skip_processing']:
-            log.info(f'Process starting at {datetime.now()}')
-            launch_dir = os.getcwd()
-            os.chdir(cfg['workdir'])
+        log.info('Processing GAMMA RTC "{0}" for "{1}"'.format(cfg['sub_name'], cfg['username']))
 
-            hyp3proclib.process(
-                cfg, 'proc_rtc_gamma', ["--hello-world"]
-            )
+        in_granule = cfg['granule']
 
-            os.chdir(launch_dir)
+        cfg['log'] = "Processing started at " + str(datetime.now()) + "\n\n"
+        g = download(cfg, in_granule)
+
+        if in_granule.startswith('S1'):
+
+            if 'RAW' in g:
+                raise Exception('Sentinel RAW data is not supported: ' + in_granule)
+            d = g[17:25]
+            sd = d[0:4] + '-' + d[4:6] + '-' + d[6:8]
+            cfg["email_text"] = "This is an RTC product from {0}.".format(sd)
+
+            res = get_extra_arg(cfg, 'resolution', cfg['default_rtc_resolution'])
+            opts_str = f'_{res}'
+
+            if res not in ('10m', '30m'):
+                raise Exception('Invalid resolution: ' + res)
+
+            if res == '30m':
+                args = ['-l']
+            else:
+                args = []
+
+            if extra_arg_is(cfg, 'power', 'no') or extra_arg_is(cfg, 'keep_area', 'yes'):
+                opts_str += '_amp'
+                args += ['--amp']
+            else:
+                opts_str += '_power'
+
+            if extra_arg_is(cfg, 'gamma0', 'no') or extra_arg_is(cfg, 'keep_area', 'yes'):
+                opts_str += '_sigma0'
+                args += ['--sigma']
+            else:
+                opts_str += '_gamma0'
+
+            if extra_arg_is(cfg, 'filter', 'yes'):
+                args += ['-f']
+                opts_str += '_filt'
+
+            if extra_arg_is(cfg, 'keep_area', 'yes'):
+                args += ['-n']
+                opts_str += '_flat'
+
+            if res == '10m':
+                args += ['-o', '10']
+            else:
+                default_looks = 6
+                if 'SLC' in in_granule:
+                    default_looks = 3
+                looks = get_extra_arg(cfg, 'looks', str(default_looks))
+                args += ['-k', looks]
+
+            safe_dir = g + '.SAFE'
+            args += [safe_dir, ]
+
+            process(cfg, 'run_new_rtc', args)
+
+        elif in_granule.startswith('E1') \
+                or in_granule.startswith('E2') \
+                or in_granule.startswith('R1') or in_granule.startswith('J1'):
+
+            if in_granule.startswith('J1') and not user_ok_for_jers(cfg, cfg['user_id']):
+                raise Exception('User does not have permission to process JERS data: ' + in_granule)
+
+            log.info('Legacy granule: ' + g)
+            raw = find_raw(cfg['workdir'])
+            d = os.path.dirname(raw)
+
+            log.info('Changing to directory ' + d)
+            os.chdir(d)
+
+            opts_str = '-12.5m'
+            args = ["-g", "-d", os.path.basename(raw)]
+
+            process(cfg, 'run_rtc_legacy', args)
+
         else:
-            log.info('Processing skipped!')
-            cfg['log'] += "(debug mode)"
+            raise Exception('Unrecognized: ' + in_granule)
 
-        cfg['success'] = True
-        hyp3proclib.update_completed_time(cfg)
-
-        product_dir = os.path.join(cfg['workdir'], 'PRODUCT')
-        if not os.path.isdir(product_dir):
-            log.info(f'PRODUCT directory not found: {product_dir}')
+        product = find_product(cfg['workdir'])
+        if not os.path.isdir(product):
+            log.info('PRODUCT directory not found: ' + product)
             log.error('Processing failed')
-            raise Exception('Processing failed: PRODUCT directory not found')
+            raise Exception("Processing failed: PRODUCT directory not found")
         else:
-            # TODO: final product cleanup and upload to HyP3 DB
-            pass
+            if extra_arg_is(cfg, "include_dem", "no"):
+                find_and_remove(product, '_dem.tif')
+            if extra_arg_is(cfg, "include_inc", "no"):
+                find_and_remove(product, '_inc_map.tif')
+
+            out_name = build_output_name(g, cfg['workdir'], opts_str + cfg['suffix'])
+            if "STD_L0_F" in out_name:
+                out_name = out_name.replace("_L0", "")
+            log.info('Output name: ' + out_name)
+
+            out_path = os.path.join(cfg['workdir'], out_name)
+            log.info('Output path: ' + out_path)
+
+            zip_file = out_path + '.zip'
+            if os.path.isdir(out_path):
+                shutil.rmtree(out_path)
+            if os.path.isfile(zip_file):
+                os.unlink(zip_file)
+            cfg['out_path'] = out_path
+
+            with get_db_connection('hyp3-db') as conn:
+                clip_tiffs_to_roi(cfg, conn, product)
+
+                log.debug('Renaming ' + product + ' to ' + out_path)
+                os.rename(product, out_path)
+
+                browse_path = find_png(out_path)
+                cfg['attachment'] = browse_path
+                add_browse(cfg, 'LOW-RES', browse_path)
+
+                find_browses(cfg, out_path)
+                add_citation(cfg, out_path)
+                zip_dir(out_path, zip_file)
+
+                cfg['final_product_size'] = [os.stat(zip_file).st_size, ]
+                cfg['original_product_size'] = 0
+
+                record_metrics(cfg, conn)
+                if 'lag' in cfg and 'email_text' in cfg:
+                    cfg['email_text'] += "\n" + "You are receiving this product {0} after it was acquired.".format(
+                        cfg['lag'])
+
+                upload_product(zip_file, cfg, conn, browse_path=browse_path)
+                success(conn, cfg)
 
     except Exception as e:
-        log.exception('rtc_gamma processing failed!')
-        log.exception('Notifying user')
-        hyp3proclib.failure(cfg, str(e))
+        log.exception('Processing failed')
+        log.info('Notifying user')
 
-    hyp3proclib.file_system.cleanup_workdir(cfg)
+        failure(cfg, str(e))
 
-    log.info('rtc_gamma done')
+    cleanup_workdir(cfg)
+
+    log.info('Done')
 
 
 def main():
@@ -56,7 +284,7 @@ def main():
     Main entrypoint for hyp3_rtc_gamma
     """
     processor = Processor(
-        'rtc_gamma', hyp3_process, sci_version=hyp3_rtc_gamma.__version__
+        'rtc_gamma', process_rtc_gamma, sci_version=hyp3_rtc_gamma.__version__
     )
     processor.run()
 
