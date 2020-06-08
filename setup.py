ae4e235411398ea37ab6f82b9fb71da2ee5350e6
Joseph H Kennedy <me@jhkennedy.org>
2020-06-08 15:16:53 -0800

    Specify hyp3lib and hyp3proclib requirements

--- setup.py
+++ setup.py
@@ -32,8 +32,8 @@ setup(
     python_requires='~=3.6',
 
     install_requires=[
-        'hyp3lib',
-        'hyp3proclib',
+        'hyp3lib>=1.2.1,<2',
+        'hyp3proclib~=1.0',
         'importlib_metadata',
         'lxml',
         'numpy',
