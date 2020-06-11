c09d7a38400f26b0298265e4ebbeade430c58e1e
Joseph H Kennedy <me@jhkennedy.org>
2020-06-11 14:34:02 -0800

    test new entry and v2 entrypoints

--- tests/test_entrypoints.py
+++ tests/test_entrypoints.py
@@ -1,3 +1,29 @@
+def test_rtc_gamma(script_runner):
+    ret = script_runner.run('rtc_gamma', '+h')
+    assert ret.success
+
+
+def test_rtc_gamma_passthrough(script_runner):
+    ret = script_runner.run('rtc_gamma', '--version')
+    assert ret.success
+    assert 'rtc_gamma v' in ret.stdout
+    assert 'hyp3lib v' in ret.stdout
+    assert 'hyp3proclib v' in ret.stdout
+
+
+def test_rtc_gamma_passthrough_v2(script_runner):
+    ret = script_runner.run(
+        'rtc_gamma', '++entrypoint', 'hyp3_rtc_gamma_v2', '--help')
+    assert ret.success
+    assert 'hyp3_rtc_gamma_v2' in ret.stdout
+    assert '--bucket-prefix' in ret.stdout
+
+
+def test_hyp3_rtc_gamma_v2(script_runner):
+    ret = script_runner.run('hyp3_rtc_gamma_v2', '-h')
+    assert ret.success
+
+
 def test_hyp3_rtc_gamma(script_runner):
     ret = script_runner.run('hyp3_rtc_gamma', '-h')
     assert ret.success
