c5f5f0a23f946c15de7fbf9b1e204e14b7493f2a
Joseph H Kennedy <me@jhkennedy.org>
2020-07-13 20:53:19 -0800

    asfdmin --> ASFHyP3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description=long_desc,
     long_description_content_type='text/markdown',
 
-    url='https://github.com/asfadmin/hyp3-rtc-gamma',
+    url='https://github.com/ASFHyP3/hyp3-rtc-gamma',
 
     author='ASF APD/Tools Team',
     author_email='uaf-asf-apd@alaska.edu',
