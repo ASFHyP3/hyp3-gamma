13ec100a7eef55b00bb07fc159237601f0fc5418
Andrew Johnston <asjohnston@alaska.edu>
2020-08-27 13:57:53 -0800

    sort input granules in v2 entrypoint

--- hyp3_gamma/insar/__main__.py
+++ hyp3_gamma/insar/__main__.py
@@ -65,16 +65,17 @@ def main_v2():
     parser.add_argument('--multilook', choices=['20x4', '10x2'], default='20x4')
     parser.add_argument('granule1')
     parser.add_argument('granule2')
-
     args = parser.parse_args()
+
+    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                        datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
 
     alooks, rlooks = (20, 4) if args.multilook == '20x4' else (10, 2)
 
     granule_file = 'granules.txt'
-    write_list_file(granule_file, args.granule1, args.granule2)
+    g1, g2 = earlier_granule_first(args.granule1, args.granule2)
+    write_list_file(granule_file, g1, g2)
 
-    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
-                        datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
     stack_sentinel.procS1StackGAMMA(
         alooks=alooks,
         rlooks=rlooks,
