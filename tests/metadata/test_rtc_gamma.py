0ad40ac8d3d893177c2b1425a137b049f4e99b70
Joseph H Kennedy <me@jhkennedy.org>
2020-09-03 16:07:48 -0800

    Add inc_map and ls_map templates

--- tests/metadata/test_rtc_gamma.py
+++ tests/metadata/test_rtc_gamma.py
@@ -61,3 +61,43 @@ def test_create_greyscale_browse_xml(tmp_path, test_data_folder):
     output_file = __main__.create_browse_xml(payload)
 
     assert output_file.exists()
+
+
+# FIXME: use tmp_path
+def test_exact_rtc_gamma_inc_map(test_data_folder):
+    product_dir = test_data_folder / 'S1A_IW_20150621T120220_SVP_RTC10_G_saufem_F8E2'
+    payload = __main__.marshal_metadata(
+        product_dir=product_dir,
+        granule_name='S1A_IW_SLC__1SSV_20150621T120220_20150621T120232_006471_008934_72D8',
+        dem_name='SRTMGL1',
+        processing_date=datetime.strptime('2020-01-01T00:00:00+0000', '%Y-%m-%dT%H:%M:%S%z'),
+        looks=1,
+        plugin_name='hyp3_rtc_gamma',
+        plugin_version='2.3.0',
+        processor_name='GAMMA',
+        processor_version='20191203',
+    )
+
+    output_file = __main__.create_inc_map_xml(payload)
+
+    assert output_file.exists()
+
+
+# FIXME: use tmp_path
+def test_exact_rtc_gamma_ls_map(test_data_folder):
+    product_dir = test_data_folder / 'S1A_IW_20150621T120220_SVP_RTC10_G_saufem_F8E2'
+    payload = __main__.marshal_metadata(
+        product_dir=product_dir,
+        granule_name='S1A_IW_SLC__1SSV_20150621T120220_20150621T120232_006471_008934_72D8',
+        dem_name='SRTMGL1',
+        processing_date=datetime.strptime('2020-01-01T00:00:00+0000', '%Y-%m-%dT%H:%M:%S%z'),
+        looks=1,
+        plugin_name='hyp3_rtc_gamma',
+        plugin_version='2.3.0',
+        processor_name='GAMMA',
+        processor_version='20191203',
+    )
+
+    output_file = __main__.create_ls_map_xml(payload)
+
+    assert output_file.exists()
