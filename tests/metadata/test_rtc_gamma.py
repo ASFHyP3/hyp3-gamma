3f36a70c9fd213ed283fc7f6865d30713e3e8ec3
Joseph H Kennedy <me@jhkennedy.org>
2020-09-09 09:59:28 -0800

    Add a VH tif to the test

--- tests/metadata/test_rtc_gamma.py
+++ tests/metadata/test_rtc_gamma.py
@@ -17,11 +17,11 @@ def test_create_rtc_gamma_readme(product_dir):
     )
 
     output_file = create.create_readme(payload)
-    assert output_file == product_dir / 'S1A_IW_20150621T120220_SVP_RTC10_G_saufem_F8E2.README.md.txt'
+    assert output_file == product_dir / 'S1A_IW_20150621T120220_DVP_RTC10_G_saufem_F8E2.README.md.txt'
     assert output_file.exists()
 
 
-def test_exact_rtc_gamma_product(product_dir):
+def test_rtc_gamma_product(product_dir):
     payload = create.marshal_metadata(
         product_dir=product_dir,
         granule_name='S1A_IW_SLC__1SSV_20150621T120220_20150621T120232_006471_008934_72D8',
@@ -35,7 +35,9 @@ def test_exact_rtc_gamma_product(product_dir):
     )
 
     output_file_list = create.create_product_xmls(payload)
-    assert output_file_list == [product_dir / 'S1A_IW_20150621T120220_SVP_RTC10_G_saufem_F8E2_VV.tif.xml']
+    assert output_file_list == [
+        product_dir / 'S1A_IW_20150621T120220_DVP_RTC10_G_saufem_F8E2_VV.tif.xml',
+        product_dir / 'S1A_IW_20150621T120220_DVP_RTC10_G_saufem_F8E2_VH.tif.xml']
     for output_file in output_file_list:
         assert output_file.exists()
 
@@ -54,7 +56,7 @@ def test_create_dem_xml(product_dir):
     )
 
     output_file = create.create_dem_xml(payload)
-    assert output_file == product_dir / 'S1A_IW_20150621T120220_SVP_RTC10_G_saufem_F8E2_dem.tif.xml'
+    assert output_file == product_dir / 'S1A_IW_20150621T120220_DVP_RTC10_G_saufem_F8E2_dem.tif.xml'
     assert output_file.exists()
 
 
@@ -72,11 +74,11 @@ def test_create_greyscale_browse_xml(product_dir):
     )
 
     output_file = create.create_browse_xml(payload)
-    assert output_file == product_dir / 'S1A_IW_20150621T120220_SVP_RTC10_G_saufem_F8E2.png.xml'
+    assert output_file == product_dir / 'S1A_IW_20150621T120220_DVP_RTC10_G_saufem_F8E2.png.xml'
     assert output_file.exists()
 
 
-def test_exact_rtc_gamma_inc_map(product_dir):
+def test_rtc_gamma_inc_map(product_dir):
     payload = create.marshal_metadata(
         product_dir=product_dir,
         granule_name='S1A_IW_SLC__1SSV_20150621T120220_20150621T120232_006471_008934_72D8',
@@ -90,11 +92,11 @@ def test_exact_rtc_gamma_inc_map(product_dir):
     )
 
     output_file = create.create_inc_map_xml(payload)
-    assert output_file == product_dir / 'S1A_IW_20150621T120220_SVP_RTC10_G_saufem_F8E2_inc_map.tif.xml'
+    assert output_file == product_dir / 'S1A_IW_20150621T120220_DVP_RTC10_G_saufem_F8E2_inc_map.tif.xml'
     assert output_file.exists()
 
 
-def test_exact_rtc_gamma_ls_map(product_dir):
+def test_rtc_gamma_ls_map(product_dir):
     payload = create.marshal_metadata(
         product_dir=product_dir,
         granule_name='S1A_IW_SLC__1SSV_20150621T120220_20150621T120232_006471_008934_72D8',
@@ -108,7 +110,7 @@ def test_exact_rtc_gamma_ls_map(product_dir):
     )
 
     output_file = create.create_ls_map_xml(payload)
-    assert output_file == product_dir / 'S1A_IW_20150621T120220_SVP_RTC10_G_saufem_F8E2_ls_map.tif.xml'
+    assert output_file == product_dir / 'S1A_IW_20150621T120220_DVP_RTC10_G_saufem_F8E2_ls_map.tif.xml'
     assert output_file.exists()
 
 
@@ -125,12 +127,13 @@ def test_rtc_gamma_all_files(product_dir):
         processor_version='20191203',
     )
     assert files == [
-        product_dir / 'S1A_IW_20150621T120220_SVP_RTC10_G_saufem_F8E2_VV.tif.xml',
-        product_dir / 'S1A_IW_20150621T120220_SVP_RTC10_G_saufem_F8E2.README.md.txt',
-        product_dir / 'S1A_IW_20150621T120220_SVP_RTC10_G_saufem_F8E2_dem.tif.xml',
-        product_dir / 'S1A_IW_20150621T120220_SVP_RTC10_G_saufem_F8E2.png.xml',
-        product_dir / 'S1A_IW_20150621T120220_SVP_RTC10_G_saufem_F8E2_inc_map.tif.xml',
-        product_dir / 'S1A_IW_20150621T120220_SVP_RTC10_G_saufem_F8E2_ls_map.tif.xml',
+        product_dir / 'S1A_IW_20150621T120220_DVP_RTC10_G_saufem_F8E2_VV.tif.xml',
+        product_dir / 'S1A_IW_20150621T120220_DVP_RTC10_G_saufem_F8E2_VH.tif.xml',
+        product_dir / 'S1A_IW_20150621T120220_DVP_RTC10_G_saufem_F8E2.README.md.txt',
+        product_dir / 'S1A_IW_20150621T120220_DVP_RTC10_G_saufem_F8E2_dem.tif.xml',
+        product_dir / 'S1A_IW_20150621T120220_DVP_RTC10_G_saufem_F8E2.png.xml',
+        product_dir / 'S1A_IW_20150621T120220_DVP_RTC10_G_saufem_F8E2_inc_map.tif.xml',
+        product_dir / 'S1A_IW_20150621T120220_DVP_RTC10_G_saufem_F8E2_ls_map.tif.xml',
     ]
     for f in files:
         assert f.exists()
