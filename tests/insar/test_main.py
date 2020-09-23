4e56a627fab55c346c18f04c0ef51bddd293da08
Andrew Johnston <asjohnston@alaska.edu>
2020-09-23 14:59:27 -0800

    use least precise orbit in product names

--- tests/insar/test_main.py
+++ tests/insar/test_main.py
@@ -3,11 +3,24 @@ from re import match
 from hyp3_insar_gamma import __main__ as main
 
 
+def test_least_precise_orbit_of():
+    precise = 'S1A_OPER_AUX_POEORB_OPOD_20160616T121500_V20160526T225943_20160528T005943'
+    restituted = 'S1B_OPER_AUX_RESORB_OPOD_20200907T115242_V20200906T042511_20200906T074241'
+    predicted = None
+    assert main.least_precise_orbit_of([precise, precise]) == 'P'
+    assert main.least_precise_orbit_of([precise, restituted]) == 'R'
+    assert main.least_precise_orbit_of([precise, predicted]) == 'O'
+    assert main.least_precise_orbit_of([restituted, restituted]) == 'R'
+    assert main.least_precise_orbit_of([restituted, predicted]) == 'O'
+    assert main.least_precise_orbit_of([predicted, predicted]) == 'O'
+
+
 def test_get_product_name():
     payload = {
         'reference_name': 'S1A_IW_SLC__1SSV_20160527T014319_20160527T014346_011438_011694_26B0',
         'secondary_name': 'S1A_IW_SLC__1SSV_20160714T014322_20160714T014349_012138_012CE7_96A0',
-        'orbit_file': 'S1A_OPER_AUX_POEORB_OPOD_20160616T121500_V20160526T225943_20160528T005943.EOF',
+        'reference_orbit': 'S1A_OPER_AUX_POEORB_OPOD_20160616T121500_V20160526T225943_20160528T005943.EOF',
+        'secondary_orbit': 'S1A_OPER_AUX_POEORB_OPOD_20160616T121500_V20160526T225943_20160528T005943.EOF',
         'pixel_spacing': 80,
         'masked': False,
     }
@@ -17,7 +30,8 @@ def test_get_product_name():
     payload = {
         'reference_name': 'S1B_IW_SLC__1SDH_20200918T073646_20200918T073716_023426_02C7FC_6374',
         'secondary_name': 'S1A_IW_SLC__1SDH_20200906T073646_20200906T073716_023251_02C278_AE75',
-        'orbit_file': 'S1B_OPER_AUX_RESORB_OPOD_20200907T115242_V20200906T042511_20200906T074241.EOF',
+        'reference_orbit': 'S1B_OPER_AUX_RESORB_OPOD_20200907T115242_V20200906T042511_20200906T074241.EOF',
+        'secondary_orbit': 'S1A_OPER_AUX_POEORB_OPOD_20160616T121500_V20160526T225943_20160528T005943.EOF',
         'pixel_spacing': 40,
         'masked': True,
     }
