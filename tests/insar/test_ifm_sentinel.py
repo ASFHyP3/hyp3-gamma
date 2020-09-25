d9b3f28b59abecfd3e7a466586cbb22f9a887766
Andrew Johnston <asjohnston@alaska.edu>
2020-09-24 21:53:51 -0800

    raise GranuleError for unsupported granule types

--- tests/insar/test_ifm_sentinel.py
+++ tests/insar/test_ifm_sentinel.py
@@ -1,6 +1,7 @@
 from re import match
 
 import pytest
+from hyp3lib import GranuleError
 
 from hyp3_insar_gamma import ifm_sentinel
 
@@ -10,13 +11,13 @@ def test_get_copol():
     assert ifm_sentinel.get_copol('S1B_IW_GRDH_1SDV_20200924T092954_20200924T093026_023515_02CABC_6C62') == 'vv'
     assert ifm_sentinel.get_copol('S1B_IW_GRDH_1SSH_20200924T112903_20200924T112932_023516_02CAC7_D003') == 'hh'
     assert ifm_sentinel.get_copol('S1B_IW_OCN__2SDH_20200924T090450_20200924T090519_023515_02CAB8_917B') == 'hh'
-    with pytest.raises(ValueError):
+    with pytest.raises(GranuleError):
         ifm_sentinel.get_copol('S1A_EW_GRDM_1SHH_20150513T080355_20150513T080455_005900_007994_35D2')
-    with pytest.raises(ValueError):
+    with pytest.raises(GranuleError):
         ifm_sentinel.get_copol('S1A_EW_GRDM_1SHV_20150509T230833_20150509T230912_005851_00787D_3BE5')
-    with pytest.raises(ValueError):
+    with pytest.raises(GranuleError):
         ifm_sentinel.get_copol('S1A_IW_SLC__1SVH_20150706T015744_20150706T015814_006684_008EF7_9B69')
-    with pytest.raises(ValueError):
+    with pytest.raises(GranuleError):
         ifm_sentinel.get_copol('S1A_IW_GRDH_1SVV_20150706T015720_20150706T015749_006684_008EF7_54BA')
 
 
