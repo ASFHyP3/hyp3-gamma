a5583200c3256233131f798e5c4da8c7d6491e38
Joseph H Kennedy <me@jhkennedy.org>
2020-10-01 14:27:54 -0800

    cleanup Jupyter config

--- etc/jupyter_notebook_config.py
+++ etc/jupyter_notebook_config.py
@@ -2,12 +2,6 @@
 # Distributed under the terms of the Modified BSD License.
 # https://github.com/jupyter/docker-stacks/blob/master/base-notebook/jupyter_notebook_config.py
 
-from jupyter_core.paths import jupyter_data_dir
-import subprocess
-import os
-import errno
-import stat
-
 c = get_config()  # noqa: F821
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8888
