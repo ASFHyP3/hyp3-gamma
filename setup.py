5d39be6fd02bed39fc5e375f46c389cc7cf6af57
Andrew Johnston <asjohnston@alaska.edu>
2021-02-15 17:58:01 -0900

    update hyp3lib to v1.6.4

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
 
     install_requires=[
         'gdal',
-        'hyp3lib>=1.6.3,<2',
+        'hyp3lib>=1.6.4,<2',
         'hyp3_metadata>=0.2.0,<1',
         'importlib_metadata',
         'lxml',
