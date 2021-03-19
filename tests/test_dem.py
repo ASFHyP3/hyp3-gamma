217765d6ad41ab85097d5bc4abb5b99735884fe6
Joseph H Kennedy <me@jhkennedy.org>
2021-03-19 14:42:47 -0800

    Watch a value change

--- tests/test_dem.py
+++ tests/test_dem.py
@@ -7,21 +7,22 @@ gdal.UseExceptions()
 
 def test_gdal_config_manager():
     gdal.SetConfigOption('OPTION1', 'VALUE1')
+    gdal.SetConfigOption('OPTION2', 'VALUE2')
 
     assert gdal.GetConfigOption('OPTION1') == 'VALUE1'
-    assert gdal.GetConfigOption('OPTION2') is None
+    assert gdal.GetConfigOption('OPTION2') == 'VALUE2'
     assert gdal.GetConfigOption('OPTION3') is None
     assert gdal.GetConfigOption('OPTION4') is None
 
-    with dem.GDALConfigManager(OPTION2='VALUE2', OPTION3='VALUE3'):
+    with dem.GDALConfigManager(OPTION2='CHANGED', OPTION3='VALUE3'):
         assert gdal.GetConfigOption('OPTION1') == 'VALUE1'
-        assert gdal.GetConfigOption('OPTION2') == 'VALUE2'
+        assert gdal.GetConfigOption('OPTION2') == 'CHANGED'
         assert gdal.GetConfigOption('OPTION3') == 'VALUE3'
         assert gdal.GetConfigOption('OPTION4') is None
 
         gdal.SetConfigOption('OPTION4', 'VALUE4')
 
     assert gdal.GetConfigOption('OPTION1') == 'VALUE1'
-    assert gdal.GetConfigOption('OPTION2') is None
+    assert gdal.GetConfigOption('OPTION2') == 'VALUE2'
     assert gdal.GetConfigOption('OPTION3') is None
     assert gdal.GetConfigOption('OPTION4') == 'VALUE4'
