2569e3afa39cd53af11c1a8be44512035e83d023
Andrew Johnston <asjohnston@alaska.edu>
2021-03-31 09:47:54 -0800

    fix import order

--- hyp3_gamma/metadata/__main__.py
+++ hyp3_gamma/metadata/__main__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from dateutil.parser import parse as dt_parse
 
 from hyp3_metadata import __version__, create_metadata_file_set
-from hyp3_metadata.util import populate_example_data, SUPPORTED_DEMS
+from hyp3_metadata.util import SUPPORTED_DEMS, populate_example_data
 
 
 def main():
