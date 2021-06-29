119ec2fcc5a34dcdbd3285320bbfe2aa51ee6fc5
Joseph H Kennedy <me@jhkennedy.org>
2021-06-29 10:58:42 -0800

    Add documentation URL for pypi

--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@ setup(
     long_description_content_type='text/markdown',
 
     url='https://github.com/ASFHyP3/hyp3-gamma',
+    project_urls={
+        'Documentation': 'https://hyp3-docs.asf.alaska.edu',
+    },
 
     author='ASF APD/Tools Team',
     author_email='uaf-asf-apd@alaska.edu',
