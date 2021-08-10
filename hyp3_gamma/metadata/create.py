c0fd02fff7f1294e1bf0665e3f3a431adbb96c11
Joseph H Kennedy <me@jhkennedy.org>
2021-08-09 16:30:09 -0800

    Update python for water mask applied option

--- hyp3_gamma/metadata/create.py
+++ hyp3_gamma/metadata/create.py
@@ -25,8 +25,9 @@ def create_metadata_file_set_rtc(product_dir: Path, granule_name: str, dem_name:
 
 
 def create_metadata_file_set_insar(product_dir: Path, reference_granule_name: str, secondary_granule_name: str,
-                                   processing_date: datetime, looks: str, dem_name: str, plugin_name: str,
-                                   plugin_version: str, processor_name: str, processor_version: str) -> List[Path]:
+                                   processing_date: datetime, looks: str, dem_name: str, water_mask_applied: bool,
+                                   plugin_name: str, plugin_version: str, processor_name: str, processor_version: str
+                                   ) -> List[Path]:
     payload = insar.marshal_metadata(
         product_dir=product_dir,
         reference_granule_name=reference_granule_name,
@@ -34,6 +35,7 @@ def create_metadata_file_set_insar(product_dir: Path, reference_granule_name: st
         processing_date=processing_date,
         looks=looks,
         dem_name=dem_name,
+        water_mask_applied=water_mask_applied,
         plugin_name=plugin_name,
         plugin_version=plugin_version,
         processor_name=processor_name,
