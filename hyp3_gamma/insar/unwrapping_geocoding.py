f319b42b78cda84b61d1102e96a54264901f90cd
jzhu4 <jzhu4@alaska.edu>
2021-10-14 14:24:11 -0800

    fix a typo

--- hyp3_gamma/insar/unwrapping_geocoding.py
+++ hyp3_gamma/insar/unwrapping_geocoding.py
@@ -61,7 +61,7 @@ def get_coords(in_mli_par: str, ref_azlin: int = 0, ref_rpix: int = 0, in_dem_pa
         coordinates dictionary with row_s, col_s, lat, lon coordinates. Additionally, if
         in_dem_par is provided, coords will have row_m, col_m, y, and x.
     """
-    row_s, col_s, lat, lon = coords_from_sapix_coord(in_mli_par, ref_azlin, ref_rpix, '_')
+    row_s, col_s, lat, lon = coords_from_sapix_coord(in_mli_par, ref_azlin, ref_rpix, '-')
     coords = {"row_s": row_s, "col_s": col_s, "lat": lat, "lon": lon}
 
     if in_dem_par:
