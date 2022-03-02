de71c497b5ce875ca98b2f628200be738d046af7
jzhu4 <jzhu4@alaska.edu>
2022-03-01 15:42:55 -0900

    imporve the ref_point_with_max_cc function to adapt both adf.cc and cc files

--- hyp3_gamma/insar/unwrapping_geocoding.py
+++ hyp3_gamma/insar/unwrapping_geocoding.py
@@ -115,7 +115,7 @@ def window_sum(data, i, j, shift=1):
     return tot
 
 
-def calc_window_sum(data_cc, rows, cols, shift):
+def calc_window_sum(data_cc, rows, cols, shift, ratio):
     '''
     rows and cols are 1D array. shift dtermine the size of window, shift=1 means 9- window, shift=2 means 16-window.
     '''
@@ -123,7 +123,12 @@ def calc_window_sum(data_cc, rows, cols, shift):
     tots = np.zeros(num, dtype=float)
     for k in range(num):
         tots[k] = window_sum(data_cc, rows[k], cols[k], shift)
-    idx = np.where(tots == tots.max())
+
+    idx = np.where(tots >=ratio*tots.max())
+    num_curr = len(idx[0])
+    if num_curr == num:
+        idx = np.where(tots == tots.max())
+
     rows = rows[idx]
     cols = cols[idx]
     return rows, cols
@@ -138,21 +143,19 @@ def ref_point_with_max_cc(fcc: str, mlines: int, mwidth: int, shift=1, ratio=0.9
         ratio = 1.0
     data_cc = read_bin(fcc, mlines, mwidth)
     data_cc_max = data_cc[data_cc < 1.0].max()
-    idx = np.where(data_cc >= ratio*data_cc_max)
+    idx = np.where((data_cc >= ratio*data_cc_max) & (data_cc <= data_cc_max))
 
-    if idx:
-        rows, cols = idx[0], idx[1]
-        while (True):
-            rows, cols = calc_window_sum(data_cc, rows, cols, shift)
-            shift += 1
-            if rows.shape[0] == 1:
-                ref_i = rows[0]
-                ref_j = cols[0]
-                break
+    rows, cols = idx[0], idx[1]
+    while (True):
+        rows, cols = calc_window_sum(data_cc, rows, cols, shift, ratio)
+        shift += 1
+        ratio = 0.5 +  ratio/2.0
+        if rows.shape[0] == 1:
+            ref_i = rows[0]
+            ref_j = cols[0]
+            break
 
-        return ref_i, ref_j
-
-    return 0, 0
+    return ref_i, ref_j
 
 
 def geocode_back(inname, outname, width, lt, demw, demn, type_):
