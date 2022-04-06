da869c736da54814fdc4c0ee0197c8c0892fc2ca
jacquelynsmale <34557291+jacquelynsmale@users.noreply.github.com>
2022-04-06 11:57:45 -0800

    Add entry point and flood_map options

--- setup.py
+++ setup.py
@@ -67,6 +67,7 @@ setup(
             'interf_pwr_s1_lt_tops_proc.py = hyp3_gamma.insar.interf_pwr_s1_lt_tops_proc:main',
             'unwrapping_geocoding.py = hyp3_gamma.insar.unwrapping_geocoding:main',
             'water_map = hyp3_gamma.__main__:water_map',
+            'flood_map = hyp3_gamma.__main__:flood_map',
         ]
     },
 
