72dad08f0092606d02ba7082648f16cf94e8e427
jacquelynsmale <34557291+jacquelynsmale@users.noreply.github.com>
2022-04-28 15:04:38 -0800

    Switch underscores to dashes

--- hyp3_gamma/__main__.py
+++ hyp3_gamma/__main__.py
@@ -224,8 +224,8 @@ def water_map():
     if args.include_flood_depth:
         execute(f'conda run -n  asf-tools flood_map {product_name}/{product_name}_FM.tif '
                 f'{product_name}/{product_name}_WM.tif {product_name}/{product_name}_WM_HAND.tif '
-                f'--estimator {args.estimator} --water_level_sigma {args.water_level_sigma} '
-                f'--known_water_threshold {args.known_water_threshold} --iterative-bounds {args.iterative_bounds[0]} '
+                f'--estimator {args.estimator} --water-level-sigma {args.water_level_sigma} '
+                f'--known-water-threshold {args.known_water_threshold} --iterative-bounds {args.iterative_bounds[0]} '
                 f'{args.iterative_bounds[1]}',
                 uselogging=True)
 
