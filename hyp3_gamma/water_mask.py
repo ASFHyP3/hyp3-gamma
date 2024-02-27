d0785ac9847b88e14aba39eecfe3556e84f1f994
Andrew Player <andrewplayer3@gmail.com>
2024-02-27 12:53:58 -0600

    rm trailing whitespace

--- hyp3_gamma/water_mask.py
+++ hyp3_gamma/water_mask.py
@@ -135,10 +135,10 @@ def create_water_mask(input_image: str, output_image: str, gdal_format='GTiff', 
         build_vrt_command = ['gdalbuildvrt', merged_vrt_path] + tiles
         subprocess.run(build_vrt_command, check=True)
         translate_command = [
-            'gdal_translate', 
-            '-co COMPRESS=LZW', 
-            '-co NUM_THREADS=all_cpus', 
-            merged_vrt_path, 
+            'gdal_translate',
+            '-co COMPRESS=LZW',
+            '-co NUM_THREADS=all_cpus',
+            merged_vrt_path,
             merged_tif_path
         ]
         subprocess.run(translate_command, check=True)
